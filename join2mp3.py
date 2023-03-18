f7eccb686824718e73a47f7128be1243b1ae0ed7
sched75 <sched@free.fr>
2023-03-18 12:45:32 +0000

    finalisation du programme version 1.0

--- join2mp3.py
+++ join2mp3.py
@@ -1,16 +1,17 @@
 import argparse
+import glob
 import os
 import sys
 from pydub import AudioSegment
+from tqdm import tqdm
 
 # Define the command-line arguments
-parser = argparse.ArgumentParser(description='Join two MP3 files',add_help=False)##argparse.ArgumentParser(add_help=False)
-parser.add_argument('file1', metavar='file1', type=str, nargs='?',
-                    help='Path of the first MP3 file to join. Use "-" for stdin.')
-parser.add_argument('file2', metavar='file2', type=str, nargs='?',
-                    help='Path of the second MP3 file to join')
-parser.add_argument('output', metavar='output', type=str, nargs='?',
-                    help='Path and name of the output MP3 file. Use "-" for stdout.')
+parser = argparse.ArgumentParser(description='Join two or more MP3 files',add_help=False)
+parser.add_argument('files', metavar='file', type=str, nargs='+',
+                    help='Paths of the MP3 files to join in the order they should be concatenated.\
+                          You can also use wildcards to specify multiple files.')
+parser.add_argument('output', metavar='output', type=str, nargs=1,
+                    help='Path and name of the output MP3 file')
 parser.add_argument('--delete', action='store_true',
                     help='Delete the original MP3 files after combining them')
 parser.add_argument('--help', action='help', default=argparse.SUPPRESS,
@@ -21,53 +22,57 @@ parser._optionals.title = "Options"
 parser._positionals.title = "Arguments"
 parser._positionals.description = "positional arguments:"
 parser._optionals.description = "optional arguments:"
-parser._positionals.help = "    file1: Path of the first MP3 file to join. Use '-' for stdin.\n\
-    file2: Path of the second MP3 file to join.\n\
-    output: Path and name of the output MP3 file. Use '-' for stdout."
-parser._optionals.help = "    -h, --help: Show this help message and exit.\n\
+parser._positionals.help = "    file: Paths of the MP3 files to join in the order they should be concatenated.\
+                          You can also use wildcards to specify multiple files."
+parser._optionals.help = "    --help: Show this help message and exit.\n\
     --delete: Delete the original MP3 files after combining them."
 
 # Parse the command-line arguments
 args = parser.parse_args()
 
-# If no arguments are given, show help
-if not any(vars(args).values()):
-    parser.print_help()
-    sys.exit()
-
 try:
-    # Load the first MP3 file from stdin or from file
-    if args.file1 == "-":
-        song1 = AudioSegment.from_file(sys.stdin.buffer, format="mp3")
-    else:
-        song1 = AudioSegment.from_file(args.file1, format="mp3")
+    # Get a list of all the files that match the input pattern
+    input_files = []
+    for file_pattern in args.files:
+        input_files += glob.glob(file_pattern)
 
-    # Load the second MP3 file from file
-    song2 = AudioSegment.from_file(args.file2, format="mp3")
+    # Sort the input files alphabetically
+    input_files = sorted(input_files)
 
-    # Concatenate the two MP3 files
-    combined_song = song1 + song2
+    # Show the output file name before starting
+    print(f"Joining files: {', '.join(input_files)}")
+    print(f"Output file: {args.output[0]}")
 
-    # Export the combined MP3 file to stdout or to a file
-    if args.output == "-":
-        combined_song.export(sys.stdout.buffer, format='mp3')
-    else:
-        combined_song.export(args.output, format='mp3')
+    # Load the first MP3 file
+    combined_song = AudioSegment.from_file(input_files[0], format="mp3")
+
+    # Concatenate the rest of the MP3 files
+    output_file = args.output[0]
+    with tqdm(total=len(input_files) - 1, desc="Joining MP3 files") as pbar:
+        for file in input_files[1:]:
+            song = AudioSegment.from_file(file, format="mp3")
+            combined_song += song
+
+            # Save the combined audio data periodically to prevent overflow
+            if len(combined_song) > 1000000:
+                combined_song.export(output_file, format='mp3')
+                combined_song = AudioSegment.from_file(output_file, format='mp3')
+
+            pbar.update(1)
+
+    # Export the combined MP3 file
+    if output_file in input_files:
+        raise Exception("Output file cannot be the same as the input files")
+    combined_song.export(output_file, format='mp3')
 
     # Ask user for confirmation before deleting files
     if args.delete:
-        if args.file1 == "-":
-            prompt = f"Do you want to delete the original file from stdin and '{args.file2}'? [y/n] "
-        else:
-            prompt = f"Do you want to delete the original files '{args.file1}' and '{args.file2}'? [y/n] "
+        prompt = f"Do you want to delete the original files? [y/n] "
         response = input(prompt).strip().lower()
 
         if response == "y":
-            if args.file1 == "-":
-                pass  # Cannot delete file from stdin
-            else:
-                os.remove(args.file1)
-            os.remove(args.file2)
+            for file in input_files:
+                os.remove(file)
         else:
             print("Original files were not deleted.")
 
@@ -76,7 +81,7 @@ except FileNotFoundError as e:
     print("Please make sure the file paths are correct and the files exist.")
 except TypeError as e:
     print(f"Error: {str(e)}")
-    print("Please make sure the input files are in MP3 format.")
+    print("Please make sure the input files are MP3 files and they exist.")
 except Exception as e:
     print(f"Error: {str(e)}")
-    print("Please check your input and output files and try again.")
+    print("Please choose a different output file name.")
